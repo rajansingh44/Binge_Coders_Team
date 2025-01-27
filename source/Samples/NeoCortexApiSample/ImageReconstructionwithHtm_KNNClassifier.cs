63540dfc6b766b62ff392a7bb264dd9372866a09
rajan singh <rajanvsingh88@gmail.com>
2025-01-27 20:10:54 +0100

    HTM commits for Input Paramaters

--- source/Samples/NeoCortexApiSample/ImageReconstructionwithHtm_KNNClassifier.cs
+++ source/Samples/NeoCortexApiSample/ImageReconstructionwithHtm_KNNClassifier.cs
@@ -12,7 +12,7 @@ using System.Text;
 
 namespace NeoCortexApiSample
 {
-    internal class ImageReconstructionwithHtm_KNNClassifier
+    internal class ImageBinarizerSpatialPattern
     {
         public string inputPrefix { get; private set; }
 
@@ -65,7 +65,92 @@ namespace NeoCortexApiSample
         /// </summary>
         /// <param name="cfg"></param>
         /// <param name="inputPrefix"> The name of the images</param>
+        /// <returns>The trained bersion of the SP.</returns>
+        //private SpatialPooler RunExperiment(HtmConfig cfg, string inputPrefix)
+        //{
 
+        //    var mem = new Connections(cfg);
+
+        //    bool isInStableState = false;
+
+        //    int numColumns = 64 * 64;
+        //    //Accessing the Image Folder form the Cureent Directory
+        //    string trainingFolder = "Sample\\TestFiles";
+        //    //Accessing the Image Folder form the Cureent Directory Foldfer
+        //    var trainingImages = Directory.GetFiles(trainingFolder, $"{inputPrefix}*.png");
+        //    //Image Size
+        //    int imageSize = 28;
+        //    //Folder Name in the Directorty 
+        //    string testName = "test_image";
+
+        //    HomeostaticPlasticityController hpa = new HomeostaticPlasticityController(mem, trainingImages.Length * 50, (isStable, numPatterns, actColAvg, seenInputs) =>
+        //    {
+        //        // Event should only be fired when entering the stable state.
+        //        if (isStable)
+        //        {
+        //            isInStableState = true;
+        //            Debug.WriteLine($"Entered STABLE state: Patterns: {numPatterns}, Inputs: {seenInputs}, iteration: {seenInputs / numPatterns}");
+        //        }
+        //        else
+        //        {
+        //            isInStableState = false;
+        //            Debug.WriteLine($"INSTABLE STATE");
+        //        }
+        //        // Ideal SP should never enter unstable state after stable state.
+        //        Debug.WriteLine($"Entered STABLE state: Patterns: {numPatterns}, Inputs: {seenInputs}, iteration: {seenInputs / numPatterns}");
+        //    }, requiredSimilarityThreshold: 0.975);
+
+        //    // It creates the instance of Spatial Pooler Multithreaded version.
+        //    SpatialPooler sp = new SpatialPooler(hpa);
+
+        //    //Initializing the Spatial Pooler Algorithm
+        //    sp.Init(mem, new DistributedMemory() { ColumnDictionary = new InMemoryDistributedDictionary<int, NeoCortexApi.Entities.Column>(1) });
+
+        //    //Image Size
+        //    int imgSize = 28;
+        //    int[] activeArray = new int[numColumns];
+
+        //    int numStableCycles = 0;
+        //    // Runnig the Traning Cycle for 5 times
+        //    int maxCycles = 5;
+        //    int currentCycle = 0;
+
+        //    while (!isInStableState && currentCycle < maxCycles)
+        //    {
+        //        foreach (var Image in trainingImages)
+        //        {
+        //            //Binarizing the Images before taking Inputs for the Sp
+        //            string inputBinaryImageFile = NeoCortexUtils.BinarizeImage($"{Image}", imgSize, testName);
+
+        //            // Read Binarized and Encoded input csv file into array
+        //            int[] inputVector = NeoCortexUtils.ReadCsvIntegers(inputBinaryImageFile).ToArray();
+
+        //            int[] oldArray = new int[activeArray.Length];
+        //            List<double[,]> overlapArrays = new List<double[,]>();
+        //            List<double[,]> bostArrays = new List<double[,]>();
+
+        //            sp.compute(inputVector, activeArray, true);
+        //            //Getting the Active Columns
+        //            var activeCols = ArrayUtils.IndexWhere(activeArray, (el) => el == 1);
+
+        //            Debug.WriteLine($"'Cycle: {currentCycle} - Image-Input: {Image}'");
+        //            Debug.WriteLine($"INPUT :{Helpers.StringifyVector(inputVector)}");
+        //            Debug.WriteLine($"SDR:{Helpers.StringifyVector(activeCols)}\n");
+        //        }
+
+        //        currentCycle++;
+
+        //        // Check if the desired number of cycles is reached
+        //        if (currentCycle >= maxCycles)
+        //            break;
+
+        //        // Increment numStableCycles only when it's in a stable state
+        //        if (isInStableState)
+        //            numStableCycles++;
+        //    }
+
+        //    return sp;
+        //}
 
         private (SpatialPooler, HtmClassifier<string, int[]>) RunExperimentWithHTMClassifier(HtmConfig cfg, string inputPrefix)
         {
@@ -86,4 +171,22 @@ namespace NeoCortexApiSample
             }, requiredSimilarityThreshold: 0.975); // Pradeep 26/01 
 
             SpatialPooler sp = new SpatialPooler(hpa);
-            sp.Init(mem, new DistributedMemory() { ColumnDictionary = new InMemoryDistributedDictionary<int, NeoCortexApi.Entities.Column>(1) });
+            sp.Init(mem, new DistributedMemory() { ColumnDictionary = new InMemoryDistributedDictionary<int, NeoCortexApi.Entities.Column>(1) }); //Rajan
+
+
+
+            HtmClassifier<string, int[]> classifier = new HtmClassifier<string, int[]>();
+
+            int[] activeArray = new int[numColumns];
+            int maxCycles = 5;
+            int currentCycle = 0;
+
+            while (!isInStableState && currentCycle < maxCycles)
+            {
+                foreach (var image in trainingImages)
+                {
+                    string inputBinaryImageFile = NeoCortexUtils.BinarizeImage($"{image}", imgSize, testName);
+                    int[] inputVector = NeoCortexUtils.ReadCsvIntegers(inputBinaryImageFile).ToArray();
+
+                    sp.compute(inputVector, activeArray, true);
+                    var activeCols = ArrayUtils.IndexWhere(activeArray, (el) => el == 1);
