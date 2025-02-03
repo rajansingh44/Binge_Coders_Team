0458f4d7ed2d1a275d610a24e7b0bd50c049798f
Pradeep Tiwari <prad.tiwari99@gmail.com>
2025-02-03 20:09:14 +0100

    KNN classifier updates

--- source/Samples/NeoCortexApiSample/ImageReconstructionwithHtm_KNNClassifier.cs
+++ source/Samples/NeoCortexApiSample/ImageReconstructionwithHtm_KNNClassifier.cs
@@ -307,7 +307,14 @@ namespace NeoCortexApiSample
             var testActiveCells = testActiveCols.Select(colIdx => new Cell { Index = colIdx }).ToArray();
 
             // Get predictions from the KNN classifier
-            var predictions = knnClassifier.GetPredictedInputValues(testActiveCells, 7); // Top 3 predictions
+            var predictions = knnClassifier.GetPredictedInputValues(testActiveCells, 7); // Top 7 predictions
+
+            // Display predictions
+            foreach (var prediction in predictions)
+            {
+                Debug.WriteLine($"Predicted Label: {prediction.PredictedInput}, Accuracy: {prediction.Similarity}");
+            }
+
 
         }
 
