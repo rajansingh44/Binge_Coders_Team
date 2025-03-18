a25cc5a48eeb41a6cc327de5ef195fab3aeace4c
Pradeep Tiwari <prad.tiwari99@gmail.com>
2025-03-18 10:50:56 +0100

    Adding logic for binarizedimage for fixed size

--- source/Samples/NeoCortexApiSample/ImageReconstructionwithHtm_KNNClassifier.cs
+++ source/Samples/NeoCortexApiSample/ImageReconstructionwithHtm_KNNClassifier.cs
@@ -495,6 +495,45 @@ namespace NeoCortexApiSample
             }
 
             return outputFile;
+
+
+                 private static string BinarizeImageToFixedSize(string imagePath, int gridSize)
+        {
+            string outputFile = Path.Combine("Output", Path.GetFileNameWithoutExtension(imagePath) + ".txt");
+
+            using (Bitmap originalImage = new Bitmap(imagePath))
+            using (Bitmap resizedImage = new Bitmap(originalImage, new Size(gridSize, gridSize)))
+            {
+                int[] binaryArray = new int[gridSize * gridSize];
+
+                for (int y = 0; y < gridSize; y++)
+                {
+                    for (int x = 0; x < gridSize; x++)
+                    {
+                        Color pixelColor = resizedImage.GetPixel(x, y);
+                        int grayValue = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+                        binaryArray[y * gridSize + x] = (grayValue > 128) ? 1 : 0;
+                    }
+                }
+
+                using (StreamWriter writer = new StreamWriter(outputFile))
+                {
+                    for (int i = 0; i < gridSize; i++)
+                    {
+                        writer.WriteLine(string.Join("", binaryArray.Skip(i * gridSize).Take(gridSize)));
+                    }
+                }
+            }
+
+            return outputFile;
         }
+
+
+        private int[] ReadBinaryTextFile(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath);
+            return lines.SelectMany(line => line.Select(c => c == '1' ? 1 : 0)).ToArray();
+        }
+    }
     }
 }
